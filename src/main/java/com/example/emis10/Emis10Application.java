70e29847b89d2e2c5e8580128a99f7d37cfadc00
150BigDream <2316624245@qq.com>
2020-02-13 20:22:45 +0800

    emis

---
+++ src/main/java/com/example/emis10/Emis10Application.java
@@ -0,0 +1,13 @@
+package com.example.emis10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emis10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Emis10Application.class, args);
+    }
+
+}
